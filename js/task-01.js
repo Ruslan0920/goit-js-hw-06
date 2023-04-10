44d06c2d5cf4dba8eacd287d60ec8eb85d0e98a0
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-10 17:02:11 +0300

    upd

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,5 @@
 const liEl = document.querySelectorAll("li.item").length;
 console.log(`Number of categories: ${liEl}`);
 
+const hTextEl = document.querySelector("h2")
+console.log(hTextEl.textContent);
