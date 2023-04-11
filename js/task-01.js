5fd6ddef30eb26540504d26a4c7685367af2bc01
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-11 16:55:46 +0300

    up

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,11 @@
 const liEl = document.querySelectorAll("li.item").length;
 console.log(`Number of categories: ${liEl}`);
 
-const hTextEl = document.querySelector("h2")
-console.log(hTextEl.textContent);
+const hTextEl = document.querySelectorAll("h2")
+console.log(`Category: ${hTextEl.textContent}`);
+
+const ulEl = document.querySelectorAll("li.item ul")
+console.log(ulEl);
+ulEl.forEach(function (el) {
+    console.log(el);
+})
