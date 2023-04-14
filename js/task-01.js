43ca91ecfbe36b04ab0f8fcf8c3e3c10bb376f09
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-14 16:46:13 +0300

    up

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,10 @@ console.log(`Number of categories: ${liEl}`);
 const hTextEl = document.querySelector("h2")
 console.log(`Category: ${hTextEl.textContent}`);
 
-const ulEl = document.querySelectorAll("li.item li") 
-console.log(ulEl);
-console.log(ulEl.length);
+const ulEl = document.querySelectorAll("li.item ul li") 
+// console.log(ulEl);
+// console.log(ulEl.length);
 ulEl.forEach(function (el) {
-    console.log(el);
+    // console.log(el);
+    console.log("ul".length);
 })
