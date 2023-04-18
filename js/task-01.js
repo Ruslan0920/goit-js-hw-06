6ba0aed3421dc16b89fbe61e56f0f430fe986e75
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-18 16:49:30 +0300

    upd

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,49 @@
-const liEl = document.querySelectorAll("li.item").length;
-console.log(`Number of categories: ${liEl}`);
+const liEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${liEl.length}`);
+// const newElement = document.querySelectorAll("h2")
+// console.log(newElement.textContent);
+// liEl.forEach(function (elem) {
+//     console.log(elem.textContent)
+//     // console.log(elem.length)
+//     })
 
-const hTextEl = document.querySelector("h2")
-console.log(`Category: ${hTextEl.textContent}`);
 
-const ulEl = document.querySelectorAll("li.item ul li") 
-console.log(ulEl);
-console.log(ulEl.length);
-ulEl.forEach(function (el) {
-    console.log(el);
+var selectors = document.querySelectorAll("h2");
+var arr = [];
+
+selectors.forEach(function(element) {
+  console.log(`Category: ${element.textContent}`);
+});
+
+console.log(arr);
+
+// const numberElemUl = document.querySelectorAll("ul")
+// console.log(numberElemUl);
+
+
+
+// const hTextEl = document.querySelectorAll("h2")
+// console.log(`Category: ${hTextEl.textContent}`);
+// console.log(hTextEl)
+
+// const ulEl = document.querySelectorAll("li.item ul li") 
+// console.log(ulEl);
+// console.log(ulEl.length);
+// ulEl.forEach(function (el) {
+//     console.log(el);
     
-})
+// })
 
-const allUl = document.querySelector("#categories")
-console.log(allUl)
-const notAllUl = allUl.childNodes;
-console.log(notAllUl)
-// const onlyUl = notAllUl.children;
+// const allUl = document.querySelectorAll(".item")
+// console.log(allUl)
+
+// const notAllUl = allUl.childNodes;
+// console.log(notAllUl)
+// const onlyUl = notAllUl.querySelectorAll("ul");
 // console.log(onlyUl)
 
-notAllUl.forEach(function (elem) {
-    console.log(elem.textContent) 
-    console.log(elem.length)
-}) 
+// notAllUl.forEach(function (elem) {
+//     console.log(elem.textContent) 
+//     // console.log(elem.length)
+// }) 
    
