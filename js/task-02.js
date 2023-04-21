9c30629351734d3b2aba5ea469ab24c70f8b181b
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-21 15:55:06 +0300

    UPD

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach(function (elem) {
+  // console.log(elem);
+
+const newTags = document.createElement("li")
+console.log(newTags);
+newTags.textContent = elem;
+console.log(newTags.textContent);
+  newTags.classList.add("item")
+  newTags.append(ingredients)
+  console.log(ingredients);
+})
