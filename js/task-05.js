815b84c0cf8ac39963f6d4f131664a35d398d22e
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-27 16:52:48 +0300

    1

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const nameUser = document.querySelector("#name-input");
+console.log(nameUser);
+
+const greeting = document.querySelector("#name-output");
+console.log(greeting);
+
+nameUser.value = greeting;
