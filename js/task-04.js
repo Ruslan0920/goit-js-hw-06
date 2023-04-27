de0fc423aa9011f95982312763c44b5baf312417
Ruslan0920 <rus_boy2002@ukr.net>
2023-04-27 16:02:04 +0300

    upd

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,8 @@
 const counter = document.querySelector("#value");
-// console.log(counter);
+
 let counterValue = 0;
 
 const minusClick = document.querySelector('button[data-action="decrement"]');
-// console.log(minusClick);
-
 const subtractValue = () => {
     counterValue -= 1;
     counter.textContent = counterValue;
@@ -13,8 +11,6 @@ const subtractValue = () => {
 minusClick.addEventListener("click", subtractValue);
 
 const plusClick = document.querySelector('button[data-action="increment"]');
-// console.log(plusClick);
-
 const addValue = () => {
     counterValue +=1;
     counter.textContent = counterValue;
