943e565d2d83f5e52a60ea87e1fa52673462e4b3
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-01 15:21:42 +0300

    Update Task5

--- js/task-05.js
+++ js/task-05.js
@@ -1,24 +1,12 @@
 const nameUserInput = document.querySelector("#name-input");
-console.log(nameUserInput);
-
 const greeting = document.querySelector("#name-output");
-console.log(greeting);
 
 nameUserInput.addEventListener('input', changeInput);
 
-
 function changeInput(event) {
-    // console.log(event.currentTarget.value);
     greeting.textContent = event.currentTarget.value;
-    // 
-    // event.preventDefault();
-    // if (event.currentTarget.value === "") {
-    //   nameUserInput.removeEventListener('input', changeInput);  
-    // }
-};
 
-function removechangeInput(event) {
-    // console.log(event.currentTarget.value);
-    nameUserInput.removeEventListener('input', changeInput);
-    greeting.textContent = "Anonymous";
-}
+    if (event.currentTarget.value === "") {
+      document.getElementById("name-output").textContent = "Anonymous";
+    }
+};
