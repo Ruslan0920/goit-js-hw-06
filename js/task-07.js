38f26b4509a8af686394972ce0243546f28ae951
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-05 16:58:12 +0300

    222

--- js/task-07.js
+++ js/task-07.js
@@ -5,6 +5,11 @@ fontSizeControl.addEventListener('range', changeFontSize);
 const textInSpan = document.querySelector('span');
 console.log(textInSpan);
 
+let range = new Range();
+console.log(range);
+// range.setStart(font-size-control, 16);
+// range.setEnd(font-size-control, 96);
+
 function changeFontSize(event) {
-    
+    event.currentTarget.style.fontSize = "16px"
 }
