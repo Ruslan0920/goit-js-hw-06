7550622e354d08c25ee031c2545c831f002f4b7f
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-11 15:41:29 +0300

    Update Task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const buttonChangeClor = document.querySelector(".change-color");
+buttonChangeClor.addEventListener('click', changeBodyColor)
+// console.log(buttonChangeClor);
+const spanBackgroundColor = document.querySelector(".color");
+// console.log(spanBackgroundColor);
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanBackgroundColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
