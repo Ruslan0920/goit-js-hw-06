669a92712ad358cdb7dd09889369d9511dc24b60
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-12 14:28:37 +0300

    upd10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const form = document.querySelector('#controls');
+console.log(form);
+form.addEventListener('click', createElements);
+
+const allChild = form.children;
+console.log(allChild);
+
+const firstChild = form.firstElementChild;
+console.log(firstChild);
+console.log(firstChild.value);
+firstChild.addEventListener('input', inputValue);
+console.log();
+
+function inputValue(eve) {
+  eve.currentTarget.value;
+  console.log(eve.currentTarget.value);
+}
+
+
+// const btnCreate = document.querySelector('button[data-create="create"]')
+// console.log(btnCreate.dataset.create);
+
+function createElements(e) {
+  // const {
+  //   element: { number}
+  // } = e.currentTarget;
+  // console.log(e.currentTarget);
+}
+
+// const inputForm = document.querySelector(".numbers")
+// // console.log(inputForm);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
