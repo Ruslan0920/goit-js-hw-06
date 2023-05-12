0220a4cd1a7cdd1babf4d9edcedffed9ff128415
Руслан <rus_boy2002@ukr.net>
2023-05-12 23:31:43 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('#controls');
 console.log(form);
-form.addEventListener('click', createElements);
+form.addEventListener('click', createBoxes);
+
+const divBoxes = document.querySelector("#boxes");
+console.log(divBoxes);
 
 const allChild = form.children;
 console.log(allChild);
@@ -9,7 +12,7 @@ const firstChild = form.firstElementChild;
 console.log(firstChild);
 console.log(firstChild.value);
 firstChild.addEventListener('input', inputValue);
-console.log();
+console.log(firstChild.value);
 
 function inputValue(eve) {
   eve.currentTarget.value;
@@ -20,12 +23,18 @@ function inputValue(eve) {
 // const btnCreate = document.querySelector('button[data-create="create"]')
 // console.log(btnCreate.dataset.create);
 
-function createElements(e) {
+function createBoxes(amount) {
+  amount = firstChild.value;
+  const newBoxses = <div class="new-boxes" wiвth="30px" heigh="30px"></div>
+  console.log(newBoxses);
+divBoxes.innerHTML = newBoxses
+
   // const {
   //   element: { number}
   // } = e.currentTarget;
   // console.log(e.currentTarget);
 }
+// amount = firstChild.value
 
 // const inputForm = document.querySelector(".numbers")
 // // console.log(inputForm);
