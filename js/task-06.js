84cc570fe18fa8a309e7d7774c233d8052fcf512
Руслан <rus_boy2002@ukr.net>
2023-05-14 23:57:38 +0300

    UPD Task6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,9 @@ const inputForm = document.querySelector("input");
 inputForm.addEventListener("blur", inputBlur);
 
 function inputBlur(event) {
-    if (event.currentTarget.value.length >= inputForm.getAttribute("data-length")) {
+    if (event.currentTarget.value.length === Number(inputForm.getAttribute("data-length"))) {
+        // console.log(event.currentTarget.value.length);
+        // console.log(Number(inputForm.getAttribute("data-length")));
         inputForm.setAttribute("class", "valid")
     } else {
         inputForm.setAttribute("class", "invalid")
