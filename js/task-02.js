436deeafcaad6f65fb11e1a72252f76b7601bc72
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-16 16:44:02 +0300

    222

--- js/task-02.js
+++ js/task-02.js
@@ -7,15 +7,21 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector("#ingredients")
-  console.log(list);
+console.log(list);
 
 ingredients.forEach(function (elem) {
-  // console.log(elem);
-  const newElements = document.createElement("li");
-  console.log(newElements);
+  console.log(elem);
+  
+const newElements = document.createElement("li");
+    // console.log(newElements);
+
   newElements.textContent = elem;
-  console.log(newElements.textContent);
+  // console.log(newElements.textContent);
+
   newElements.classList.add("item");
+
   list.append(newElements);
-  console.log(newElements);
-})
+  // console.log(newElements);
+});
+  // list.append(createAllElements);
+  // console.log(createAllElements);
