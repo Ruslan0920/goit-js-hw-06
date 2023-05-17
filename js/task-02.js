d6e26c05e1bcf8438d7bb580e669ecfaf007e125
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-17 15:22:23 +0300

    22

--- js/task-02.js
+++ js/task-02.js
@@ -9,20 +9,28 @@ const ingredients = [
 const list = document.querySelector("#ingredients")
 console.log(list);
 
-// list.insertAdjacentHTML('beforebegin', ingredients.map(el => `<li>${el}</li>`).join(''));
+// list.insertAdjacentHTML('beforebegin', ingredients
+// .map(el => `<li>${el}</li>`)
+// .join(''));
 
+// const newElements = document.createElement("li");
+//     console.log(newElements);
 
 ingredients.forEach(function (elem) {
   console.log(elem);
+
+  const newElements = document.createElement("li");
+    console.log(newElements);
   
-const newElements = document.createElement("li");
-    // console.log(newElements);
+  // newElements = elem;
 
   newElements.textContent = elem;
   // console.log(newElements.textContent);
 
   newElements.classList.add("item");
 
-  list.append(newElements);
-  // console.log(newElements);
+  
 });
+
+// list.append(newElements);
+//   // console.log(newElements);
