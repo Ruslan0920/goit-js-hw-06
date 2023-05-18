5921f1a5601afae8dfe210795d10e6b1c99b2cef
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-18 16:08:17 +0300

    upd08

--- js/task-08.js
+++ js/task-08.js
@@ -6,9 +6,17 @@ function confirmSubmit(event) {
     const {
         elements: { email, password }
     } = event.currentTarget;
+  // console.log(event.currentTarget);
     if (email.value === "" || password.value === "") {
     return alert("Всі поля повинні бути заповнені!");
+    }
+  const objectForm = {
+    email: email.value,
+    password: password.value,
   }
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  console.log(objectForm);
+  // console.log(email.value);
+  // console.log(password.value);
+  // console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
