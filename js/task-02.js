a04d5d7aa97d5a485f1e3c17d5611b33d1fdc283
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-19 15:49:05 +0300

    2222

--- js/task-02.js
+++ js/task-02.js
@@ -7,36 +7,38 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector("#ingredients")
-// console.log(list);
+console.log(list);
 
 // list.insertAdjacentHTML('beforebegin', ingredients
 // .map(el => `<li>${el}</li>`)
 // .join(''));
 
-// const newElements = document.createElement("li");
-//     console.log(newElements);
-
-// const allElements = [];
-// console.log(allElements);
-  
+const newElements = document.createElement("li");
+//   console.log(newElements);
 ingredients.forEach(function (elem) {
-    console.log(elem);
-    
-
-  const newElements = document.createElement("li");
-    // console.log(newElements);
-  
-  
-  // newElements = elem;
+    // console.log(elem);
+// const content = newElements.innerHTML;
+// console.log(content);
+  // const newElements = document.createElement("li");
+//   console.log(newElements);
+  const createNewElements = newElements.querySelector("li");
+  console.log(createNewElements);
 
   newElements.textContent = elem;
-  // // console.log(newElements.textContent);
+  console.log(newElements.textContent);
 
   newElements.classList.add("item");
     
-  list.append(newElements);
-  // console.log(newElements);
+  // list.append(newElements);
+  // // console.log(newElements);
     // console.log(createNewElements);
+  
 });
-  // list.append();
-  // console.log(list.append(createNewElements)); 
+list.append(newElements);
+// const content = createNewElements.innerHTML;
+// console.log(content);
+//   list.append(content);
+  // console.log(list.append(createNewElements));
+// list.append(ingredients);
+// ingredients.join("<br>")
+//   console.log(ingredients.join(" "));
