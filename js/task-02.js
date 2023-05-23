1bf39c216ab7438a472d204f4aac84dcac3c21f9
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-23 11:58:21 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -10,6 +10,21 @@ const ingredients = [
 const list = document.querySelector("#ingredients");
 // console.log(list);
 
+const createNewElements = ingredients.map((ingredient) => {
+  const newElements = document.createElement('li');
+  // console.log(newElements);
+  newElements.textContent = ingredient;
+  // console.log(newElements.textContent);
+  newElements.classList.add("item");
+  
+  return newElements;
+  
+});
+
+list.append(...createNewElements);
+// console.dir(createNewElements);
+
+
 // const createNewElements = [];
 
 // ingredients.map((ingredient) => {
@@ -24,19 +39,4 @@ const list = document.querySelector("#ingredients");
 // });
 
 // list.append(...createNewElements);
-// console.dir(createNewElements);
-
-
-const createNewElements = ingredients.map((ingredient) => {
-  const newElements = document.createElement('li');
-  // console.log(newElements);
-  newElements.textContent = ingredient;
-  // console.log(newElements.textContent);
-  newElements.classList.add("item");
-  
-  return newElements;
-  
-});
-
-list.append(...createNewElements);
 // console.dir(createNewElements);
