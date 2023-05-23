fddd7da6bee37ade08ccd91a5c86b20d789ab273
Ruslan0920 <rus_boy2002@ukr.net>
2023-05-23 11:57:30 +0300

    UPD Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -7,18 +7,36 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector("#ingredients")
+const list = document.querySelector("#ingredients");
+// console.log(list);
 
-const createNewElements = [];
+// const createNewElements = [];
 
-ingredients.map((ingredient) => {
+// ingredients.map((ingredient) => {
+//   const newElements = document.createElement('li');
+//   console.log(newElements);
+//   newElements.textContent = ingredient;
+//   console.log(newElements.textContent);
+//   newElements.classList.add("item");
+  
+//   return createNewElements.push(newElements)
+  
+// });
+
+// list.append(...createNewElements);
+// console.dir(createNewElements);
+
+
+const createNewElements = ingredients.map((ingredient) => {
   const newElements = document.createElement('li');
+  // console.log(newElements);
   newElements.textContent = ingredient;
+  // console.log(newElements.textContent);
   newElements.classList.add("item");
   
-  return createNewElements.push(newElements)
+  return newElements;
   
 });
 
 list.append(...createNewElements);
-// console.log(createNewElements);
+// console.dir(createNewElements);
